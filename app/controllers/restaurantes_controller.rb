0b445db3bfc32fcf18e81c554a75291355f0efb1
leo zapparoli <leo.zapparoli@gmail.com>
2014-09-28 01:21:43 -0300

    partials, edit/update e json/xml

--- app/controllers/restaurantes_controller.rb
+++ app/controllers/restaurantes_controller.rb
@@ -1,10 +1,47 @@
 class RestaurantesController < ApplicationController
 	def index
 		@restaurantes = Restaurante.order :nome
+		respond_to do |format| 
+			format.xml {render xml: @restaurantes}
+			format.json {render json: @restaurantes}						
+			format.html 
+		end
+	end
+
+	def create
+		@restaurante = Restaurante.new(restaurante_params)
+		if @restaurante.save
+			redirect_to action: "show", id: @restaurante
+		else
+			render action: "new"
+		end
+	end
+
+	def edit
+		@restaurante = Restaurante.find(params[:id])
+	end
+
+	def update
+		@restaurante = Restaurante.find params[:id]
+		if @restaurante.update_attributes restaurante_params
+			redirect_to action: "show", id: @restaurante
+		else
+			render action: "edit"
+		end
+	end
+
+	def restaurante_params
+		params.require(:restaurante).permit(:nome,:endereco,:especialidade)
 	end
 
 	def show
 		@restaurante = Restaurante.find(params[:id])
+
+		respond_to do |format| 
+			format.xml {render xml: @restaurante}
+			format.json {render json: @restaurante}						
+			format.html 
+		end
 	end
 
 	def destroy
